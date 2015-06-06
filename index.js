f8c6232d14f1caaf90b6e246ef4acdd52f909e40
Evgeniy <w.0.w@mail.ru>
2015-06-06 20:15:07 +0300

    commit

--- index.js
+++ index.js
@@ -1,187 +1,193 @@
-var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path= require('path');
-var passport= require('passport');
-var VKontakteStrategy = require('passport-vkontakte').Strategy;
-var VK = require('vksdk');
-var mysql      = require('mysql');
+var express = require('express'),
+    app = require('express')(),
+    config = require('./config.json'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    path= require('path'),
+    passport= require('passport'),
+    VKontakteStrategy = require('passport-vkontakte').Strategy,
+    VK = require('vksdk'),
+    mysql = require('mysql'),
+    async = require('async'),
+    SessionStore = require('express-mysql-session')
 
-var cookieParser = require('cookie-parser'),
+    cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     session = require('express-session');
 
+var globalUser;
 
-app.set('view engine', 'ejs');  //tell Express we're using EJS
-app.use(cookieParser());
-app.use(session({secret: 'music', saveUninitialized: true, resave: true}));
-app.use(passport.initialize());
-app.use(passport.session());
+// connect to db
+  var connection = mysql.createConnection('mysql://'+config.connection.user+':'+config.connection.password+'@'+config.connection.host+'/'+config.connection.db);
+  connection.connect();
 
+// init app
 
+  app.set('view engine', 'ejs');  //tell Express we're using EJS
+  app.use(cookieParser());
 
-app.use(function(req, res, next){
-  var err = req.session.error,
-      msg = req.session.notice,
-      success = req.session.success;
+  var sessionStore = new SessionStore({}, connection);
 
-  delete req.session.error;
-  delete req.session.success;
-  delete req.session.notice;
+  app.use(session({
+      key: 'MUSIC',
+      secret: 'music app',
+      store: sessionStore,
+      resave: true,
+      saveUninitialized: true
+  }));
 
-  if (err) res.locals.error = err;
-  if (msg) res.locals.notice = msg;
-  if (success) res.locals.success = success;
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  next();
-});
+  app.use('/js', express.static(__dirname + '/js'));
+  app.use('/site', express.static(__dirname + '/site'));
+  app.use('/fonts', express.static(__dirname + '/fonts'));
+  app.set('views', __dirname + '/views');
 
-var connection = mysql.createConnection('mysql://root:@localhost/music');
-connection.connect();
-
-var vk = new VK({
-   'appId'     : 4935725,
-   'appSecret' : 'K2U90rURV6lJ0jN1Wx7n',
-   'language'  : 'ru'
-});
-vk.setSecureRequests(true);
+  http.listen(config.port, function(){
+      console.log('listening on *:' + config.port);
+  });
 
 
-// used to serialize the user for the session
+
+
+
+// vk sdk api
+  var vk = new VK({
+   'appId'     : config.vk.clientID,
+   'appSecret' : config.vk.clientSecret,
+   'language'  : 'ru',
+   'secure'    : true
+  });
+
+
+
+// passport CONFIG
   passport.serializeUser(function(user, done) {
       done(null, user.id);
   });
-
-  // used to deserialize the user
   passport.deserializeUser(function(id, done) {
     connection.query('SELECT * from user where id = ?', [id], function(err, user) {
       vk.setToken(user[0].token);
+      globalUser = user[0];
       done(err, user[0]);
     });
   });
 
-passport.use(new VKontakteStrategy({
-    clientID:     '4935725', // VK.com docs call it 'API ID'
-    clientSecret: 'K2U90rURV6lJ0jN1Wx7n',
-    callbackURL:  "http://192.168.0.228:3000/auth/vkontakte/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
+  passport.use(new VKontakteStrategy(config.vk,
+    function(accessToken, refreshToken, profile, done) {
 
-    connection.query('SELECT * from user where id = ?', [profile.id], function(err, user) {
-      if (err) throw err;
+      connection.query('SELECT * from user where vk_id = ?', [profile.id], function(err, user) {
+        if (err) throw err;
 
-      if (user.length) {
-          return done(null, user[0]);
-      } else {
+        if (user.length) {
+            return done(null, user[0]);
+        } else {
 
-          connection.query('INSERT INTO user SET ?', {vk_id: profile.id, fio: profile.displayName, token: accessToken}, function(err, result) {
-            if (err) throw err;
-
-            connection.query('SELECT * from user where id = ?', [result.insertId], function(err, user) {
+            connection.query('INSERT INTO user SET ?', {vk_id: profile.id, fio: profile.displayName, token: accessToken}, function(err, result) {
               if (err) throw err;
 
-              return done(null, user[0]);
-             });
-          });
-      }
-    });
-  }
-));
-
-
-app.get('/auth/vkontakte',
-  passport.authenticate('vkontakte'),
-  function(req, res){
-    // The request will be redirected to vk.com for authentication, so
-    // this function will not be called.
-  });
-
-app.get('/auth/vkontakte/callback',
-  passport.authenticate('vkontakte', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-
-
-
-
-app.get('/search', function(req, res){
-  vk.request('audio.search', {q: req.query.q}, function(_o) {
-
-    var items = _o.response.items;
-
-    for (var i = 0; i <= items.length - 1; i++) {
-      items[i].id = 'id_' + items[i].id;
+              connection.query('SELECT * from user where id = ?', [result.insertId], function(err, user) {
+                if (err) throw err;
+                return done(null, user[0]);
+               });
+            });
+        }
+      });
     }
+  ));
 
-      res.send(items);
+
+// routes
+  app.get('/auth/vkontakte',
+    passport.authenticate('vkontakte'),
+    function(req, res){
+      // The request will be redirected to vk.com for authentication, so
+      // this function will not be called.
+    });
+
+  app.get('/auth/vkontakte/callback',
+    passport.authenticate('vkontakte', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    });
+
+  app.get('/search', isLoggedIn, function(req, res){
+    vk.request('audio.search', {q: req.query.q}, function(_o) {
+
+      var items = _o.response.items;
+
+      for (var i = 0; i <= items.length - 1; i++) {
+        items[i].id = 'id_' + items[i].id;
+      }
+
+        res.send(items);
+    });
   });
-});
 
 
-app.post('/get_own_list', function(req, res){
+  app.post('/get_own_list', isLoggedIn, function(req, res){
 
-	vk.request('audio.get', {}, function(_o) {
+    // res.send([]);
 
-		var items = _o.response.items;
+  	vk.request('audio.get', {}, function(_o) {
 
-		for (var i = 0; i <= items.length - 1; i++) {
-			items[i].id = 'id_' + items[i].id;
-		}
+  		var items = _o.response.items;
 
-	    res.send(items);
-	});
-});
+  		for (var i = 0; i <= items.length - 1; i++) {
+  			items[i].id = 'id_' + items[i].id;
+  		}
 
-
-app.get('/', isLoggedIn, function(req, res){
-  res.render('index', { user: req.user });
-});
-
-app.get('/login', function(req, res){
-  res.render('login');
-});
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-}
-
-
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/site', express.static(__dirname + '/site'));
-app.use('/fonts', express.static(__dirname + '/fonts'));
-app.set('views', __dirname + '/views');
-
-http.listen(3000, function(){
-  	console.log('listening on *:3000');
-});
-
-
-io.on('connection', function(socket){
-  	console.log('a user connected');
-
-
-
-  	socket.on('add to playlist', function(audio){
-        connection.query('INSERT INTO playlist SET ?', {object: JSON.stringify(audio)}, function(err, result) {
-          if (err) throw err;
-
-          console.log(audio);
-          io.emit('add to playlist', audio);
-        });
-
-
+  	    res.send(items);
   	});
-});
+  });
 
-// route for logging out
-app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-});
+
+  app.get('/', isLoggedIn, function(req, res) {
+
+    var user = req.user;
+        delete user['token'];
+
+    async.parallel({
+        playlist: function(callback){
+          connection.query('SELECT * from playlist', function(err, playlist) {
+            var plays = [];
+
+            for (var i = 0; i <= playlist.length - 1; i++) {
+              var audio = JSON.parse(playlist[i].object);
+
+              if (playlist[i].user) {
+                var us = JSON.parse(playlist[i].user);
+                audio.user_id = us.id;
+                audio.fio = us.fio;
+              }
+              
+              plays.push(audio);
+            };
+            callback(null, plays);
+          });
+        },
+    },
+    function(err, results) {
+        // results is now equals to: {one: 1, two: 2}
+        res.render('index', { 
+          user: user,
+          playlist: results.playlist
+        });
+    });
+
+    
+  });
+
+  app.get('/login', function(req, res){
+    res.render('login');
+  });
+
+  app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
 
 
 // route middleware to make sure a user is logged in
@@ -194,3 +200,45 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/login');
 }
+
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('add to playlist', function(audio){
+        connection.query('INSERT INTO playlist SET ?', {audio_id: audio.id, object: JSON.stringify(audio), user: JSON.stringify(globalUser)}, function(err, result) {
+          if (err) throw err;
+
+          audio.user_id = globalUser.id;
+          audio.fio = globalUser.fio;
+
+          io.emit('add to playlist', audio);
+        });
+    });
+
+    socket.on('remove from playlist', function(audio, index){
+        connection.query('DELETE FROM playlist WHERE audio_id = ?', [audio.id], function(err, result) {
+          if (err) throw err;
+          // console.log(audio);
+          io.emit('remove from playlist', audio, index);
+        });
+    });
+
+    socket.on('set current', function(audio_id){
+        connection.query('UPDATE playlist SET current = 0', function(err, result) {
+          if (err) throw err;
+          connection.query('UPDATE playlist SET current = 1 where audio_id = ?', [audio_id], function(err, result) {
+            if (err) throw err;
+            io.emit('set current', audio_id);
+            connection.query('select id from playlist where current = 1', function(err, result) {
+              if (err) throw err;
+              connection.query('DELETE FROM playlist WHERE id < ?', [result[0].id - 5], function(err, result) {
+                if (err) throw err;
+              });
+            });
+          });
+        });
+    });
+
+});
--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 var socket = io();
-
-
-
-
+var notice = false;
 
 var vk = angular.module('vk', [
     "ngSanitize",
-    "angularSoundManager"
+    "angularSoundManager",
+    // "angular-sortable-view"
 ]);
 
 vk.factory('getAudioList', function($http) {
@@ -32,38 +30,70 @@ vk.factory('getAudioList', function($http) {
     return audio;
 });
 
-vk.filter('secondsToDateTime', [function() {
-    return function(seconds) {
-        var date = new Date(null);
-		date.setSeconds(seconds);
+vk.filter('secTime', function () {
+    return function (input) {
+        function pad(d) {
+            return (d < 10) ? '0' + d.toString() : d.toString();
+        }
 
-		if (date.getUTCHours() != 0) {
-			return  date.getUTCHours() + ':' + date.getUTCMinutes() + ':' +  date.getUTCSeconds();
-		} else {
-			return  date.getUTCMinutes() + ':' +  date.getUTCSeconds();
-		}
+        var min = (input / 60) << 0,
+            sec = Math.round((input % 60));
+
+        return pad(min) + ':' + pad(sec);
     };
-}]);
+});
 
 vk.controller('VkCtrl', ['$scope', '$sce', 'getAudioList', 'angularPlayer',
     function($scope, $sce, getAudioList, angularPlayer) {
 
+    	getAudioList.getList().success(function(response) {
+    	    $scope.audios = response;
+    	});
+
+    	$scope.$on('track:id', function(event, data) {
+    		socket.emit('set current', data);
+        });
+
+    	soundManager.setup({
+    	    onready: function() {
+    		  	if (playlist.length) {
+    		  		angular.forEach(playlist, function(audio, key) {
+					  	angularPlayer.addTrack(audio);
+					});
+    		  	}
+    	    },
+    	});
+
     	$scope.playlist = [];
 
-	    getAudioList.getList().success(function(response) {
-	        $scope.audios = response;
-	    });
+	    
 
 	    socket.on('add to playlist', function(audio){
-
 	    	if ($scope.getById($scope.playlist, audio.id) === undefined) {
 	    		angularPlayer.addTrack(audio);
-	    		$scope.$apply();
 	    	}
 	    });
 
+	    socket.on('remove from playlist', function(audio, index){
+	    	angularPlayer.removeSong(audio.id, index);
+	    });
+
+	    socket.on('set current', function(audio_id){
+	    	var audio = angularPlayer.currentTrackData();
+            $scope.$apply(function() {
+                $scope.current = audio_id;
+                if (notice == false) {
+                	notifyMe(audio);
+                }
+            });
+	    });
+
+
 		$scope.addToPlaylist = function(audio) {
 			socket.emit('add to playlist', audio);
+		};
+		$scope.removeFromPlaylist = function(audio, index) {
+			socket.emit('remove from playlist', audio, index);
 		};
 
 
@@ -88,3 +118,18 @@ vk.controller('VkCtrl', ['$scope', '$sce', 'getAudioList', 'angularPlayer',
 
 
 
+
+function notifyMe(audio) {
+		notice = true;
+	  	Notification.requestPermission(function(permission){
+			var notification = new Notification(audio.title,{body:audio.artist,icon:'site/logo.png',dir:'auto'});
+			setTimeout(function(){
+				notification.close();
+				notice = false;
+			},2000);
+		});
+
+
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+}
